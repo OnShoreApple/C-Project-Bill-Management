c27cf3d092fd8c6cedabbd7319bdf59b84f1ce1c
OnShoreApple <165542537+OnShoreApple@users.noreply.github.com>
2024-04-24 15:19:55 +0530

    login.c

--- login.c
+++ login.c
@@ -1,46 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-#define MAX_USERS 100
-#define MAX_USERNAME_LENGTH 50
-#define MAX_PASSWORD_LENGTH 50
+#define MAX_LENGTH 50
 
-int userExists(char *username) {
-    return 0;
+typedef struct {
+    char username[MAX_LENGTH];
+    char password[MAX_LENGTH];
+} User;
+
+User *loginUser() {
+    char username[MAX_LENGTH];
+    char password[MAX_LENGTH];
+    int i = 0;
+
+    printf("Enter username: ");
+    scanf("%49s", username);
+    printf("Enter password: ");
+    scanf("%49s", password);
+
+    User *user = (User *)malloc(sizeof(User));
+    if (user == NULL) {
+        printf("Memory allocation failed.\n");
+        exit(1);
+    }
+
+    for (i = 0; i < MAX_LENGTH && username[i] != '\0'; ++i) {
+        user->username[i] = username[i];
+    }
+    user->username[i] = '\0';
+
+    for (i = 0; i < MAX_LENGTH && password[i] != '\0'; ++i) {
+        user->password[i] = password[i];
+    }
+    user->password[i] = '\0';
+
+    return user;
 }
 
 void createUser(char *username, char *password) {
     printf("User created successfully!\n");
-}
-
-int verifyLogin(char *username, char *password) {
-    return 1;
-}
-
-int loginUser() {
-    char username[MAX_USERNAME_LENGTH];
-    char password[MAX_PASSWORD_LENGTH];
-
-    printf("Enter username: ");
-    scanf("%s", username);
-
-    if (userExists(username)) {
-        printf("Enter password: ");
-        scanf("%s", password);
-        if (verifyLogin(username, password)) {
-            printf("Login successful!\n");
-            return 1;
-        } else {
-            printf("Invalid password.\n");
-            return 0;
-        }
-    }
-    else {
-        printf("User does not exist. Creating new account...\n");
-        printf("Enter password for new account: ");
-        scanf("%s", password);
-        createUser(username, password);
-        return 1;
-    }
 }
