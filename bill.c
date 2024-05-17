34cb8ec8a4cb165d2948be8979859aa8243d2028
OnShoreApple <165542537+OnShoreApple@users.noreply.github.com>
2024-05-17 21:02:19 +0530

    Update bill.c

--- bill.c
+++ bill.c
@@ -2,28 +2,15 @@
 
 float getItemPrice(int itemId);
 
-typedef struct {
-    int id;
-    char name[50];
-    float price;
-    int quantity;
-} Item;
-
-float generateBill(Item *items, int *itemIds, int *quantities, int itemCount) {
-    float total = 0.0;
+float generateBill(int *itemIds, int *quantities, int itemCount) {
     printf("Generating Bill...\n");
     printf("Item\t\tQuantity\tPrice\n");
     printf("--------------------------------------------\n");
+    float total = 0.0;
     for (int i = 0; i < itemCount; ++i) {
         float price = getItemPrice(itemIds[i]);
-        int availableQuantity = items[itemIds[i] - 1].quantity;
-        if (quantities[i] > availableQuantity) {
-            printf("Error: Not enough stock for Item %d\n", itemIds[i]);
-            continue;
-        }
         printf("Item %d\t\t%d\t\tRs. %.2f\n", itemIds[i], quantities[i], price);
         total += price * quantities[i];
-        items[itemIds[i] - 1].quantity -= quantities[i];
     }
     printf("--------------------------------------------\n");
     printf("Total\t\t\t\tRs. %.2f\n", total);
